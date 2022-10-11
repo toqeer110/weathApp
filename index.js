3a6a7dae85c34596cb64dc89cd615d383335aae2
babayaga <toqeerali77@gmail.com>
2022-10-11 20:33:10 +0500

    git init

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+const path = require('path');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+
+app.get('/', (req, res) => {
+
+        res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/', (req, res) => {
+        const query = req.body.city;
+        const appid = '9bbdfc600023316748dcebe20a52e3ae';
+        const unit = 'metric';
+        const url = " https://api.openweathermap.org/data/2.5/weather?q=" + query + "&id=2172797&appid=" + appid + "&units=" + unit;
+        https.get(url, (response) => {
+                console.log(response.statusCode);
+
+
+                response.on('data', (data) => {
+                        const weatherData = (JSON.parse(data));
+                        const temp = weatherData.main.temp;
+                        const description = weatherData.weather[0].description;
+                        const icon = weatherData.weather[0].icon;
+                        const imgurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                        res.write("<h1>the temp of " + query + ":" + temp + " degree celcies.</h1>");
+                        res.write("<p>Description of Weather:" + description + " </p>");
+                        res.write("<img src=" + imgurl + ">");
+                        res.send();
+
+                });
+
+        });
+
+
+});
+
+
+app.listen(4545, (req, res) => {
+        console.log("server is live now");
+});
